3a7edc2cd9b2b5d0ecdaa66bcd929eaf5bc998be
{o} <b.szypelow@orb.net.pl>
2014-04-11 19:05:35 +0200

    create basic data source skeleton

---
+++ BitcoinCharts/BitcoinChartsWP/Models/Bitstamp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcoinChartsWP.Models
+{
+	public class Bitstamp : IObservable<decimal>
+	{
+		private Subject<decimal> subject = new Subject<decimal>();
+
+		public IDisposable Subscribe(IObserver<decimal> observer)
+		{
+			return this.subject.Subscribe(observer);
+		}
+	}
+}
---
+++ BitcoinCharts/BitcoinChartsWP/Models/Candle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcoinChartsWP.Models
+{
+	public class Candle
+	{
+		public decimal Hi { get; set; }
+		public decimal Lo { get; set; }
+		public decimal Open { get; set; }
+		public decimal Close { get; set; }
+	}
+}
