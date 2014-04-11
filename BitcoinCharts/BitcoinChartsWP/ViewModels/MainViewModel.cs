d25bfb0b9d35dfc204ab37ea0e7c423682457b1b
{o} <b.szypelow@orb.net.pl>
2014-04-11 17:52:21 +0200

    Basic project setup

---
+++ BitcoinCharts/BitcoinChartsWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using BitcoinChartsWP.Resources;
+
+namespace BitcoinChartsWP.ViewModels
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		public MainViewModel()
+		{
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged(String propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (null != handler)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
