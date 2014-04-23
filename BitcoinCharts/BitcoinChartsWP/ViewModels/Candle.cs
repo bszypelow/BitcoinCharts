ae9d935132f5dfa4284edef54888fc9a0bee7d2f
{o} <b.szypelow@orb.net.pl>
2014-04-23 16:44:56 +0200

    implement view model

---
+++ BitcoinCharts/BitcoinChartsWP/ViewModels/Candle.cs
@@ -0,0 +1,30 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcoinChartsWP.ViewModels
+{
+	public class Candle : ReactiveObject
+	{
+		private ObservableAsPropertyHelper<decimal> hi;
+		private ObservableAsPropertyHelper<decimal> lo;
+		private ObservableAsPropertyHelper<decimal> open;
+		private ObservableAsPropertyHelper<decimal> close;
+
+		public decimal Hi { get { return hi.Value; } }
+		public decimal Lo { get { return lo.Value; } }
+		public decimal Open { get { return open.Value; } }
+		public decimal Close { get { return close.Value; } }
+
+		public Candle(IObservable<decimal> hi, IObservable<decimal> lo, IObservable<decimal> open, IObservable<decimal> close)
+		{
+			this.hi = new ObservableAsPropertyHelper<decimal>(hi, v => raisePropertyChanged("Hi"), 0);
+			this.lo = new ObservableAsPropertyHelper<decimal>(lo, v => raisePropertyChanged("Lo"), 0);
+			this.open = new ObservableAsPropertyHelper<decimal>(open, v => raisePropertyChanged("Open"), 0);
+			this.close = new ObservableAsPropertyHelper<decimal>(close, v => raisePropertyChanged("Close"), 0);
+		}
+	}
+}
--- BitcoinCharts/BitcoinChartsWP/ViewModels/MainViewModel.cs
+++ BitcoinCharts/BitcoinChartsWP/ViewModels/MainViewModel.cs
@@ -4,16 +4,40 @@ using System.ComponentModel;
 using BitcoinChartsWP.Resources;
 using ReactiveUI;
 using BitcoinChartsWP.Models;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Threading;
+using System.Reactive.Concurrency;
 
 namespace BitcoinChartsWP.ViewModels
 {
 	public class MainViewModel : ReactiveObject
 	{
-		public MainViewModel()
+		public ObservableCollection<Candle> Candles { get; set; }
+
+		public MainViewModel(IObservable<Trade> source)
 		{
-			new Bitstamp().Subscribe(x =>
+			this.Candles = new ObservableCollection<Candle>();
+
+			var scheduler = new HistoricalScheduler(DateTimeOffset.UtcNow.AddHours(-1).AddMinutes(-1));
+			source.Subscribe(t => scheduler.AdvanceTo(t.Timestamp));
+
+			var windows = source
+				.Select(t => t.Price)
+				.Window(TimeSpan.FromMinutes(5), scheduler);
+
+			var candles = windows.Select(w => new
 				{
-					return;
+					Lo = w.Scan(0m, (min, current) => min > current ? current : min),
+					Hi = w.Scan(0m, (max, current) => max < current ? current : max),
+					Open = w.Take(1),
+					Close = w
+				});
+
+			candles.Subscribe(c =>
+				{
+					this.Candles.Add(new Candle(c.Hi, c.Lo, c.Open, c.Close));
 				});
 		}
 	}
